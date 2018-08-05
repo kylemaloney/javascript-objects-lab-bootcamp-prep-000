c728c834219e825002acc6cbacb4e9b9aec4d9cf
kylemaloney17@gmail.com <kylemaloney17@gmail.com>
2018-08-05 02:58:07 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+var recipes = new Object({key: 'value'});
+
+function updateObjectWithKeyAndValue(object, key, value){
+  object = Object.assign({}, object, {[key]: value});
+  return object;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+}
